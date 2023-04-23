74e6727a50237d62e91e0bdda7f33ec5b8451979
mgl <1375022007@qq.com>
2023-04-23 15:56:11 +0800

    add build tree

--- src/树/二叉树.js
+++ src/树/二叉树.js
@@ -13,3 +13,73 @@ var maxPathSum = function(root) {
   oneSideMax(root)
   return res;
 };
+
+// leetcode 105 构造二叉树
+// https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+/**
+ * 存储 inorder 中值到索引的映射
+ */
+let valToIndex = new Map();
+
+/**
+ * 定义：前序遍历数组为 preorder[preStart..preEnd]，
+ * 中序遍历数组为 inorder[inStart..inEnd]，
+ * 构造这个二叉树并返回该二叉树的根节点
+ */
+function build(preorder, preStart, preEnd, inorder, inStart, inEnd) {
+  if (preStart > preEnd) {
+    return null;
+  }
+
+  // root 节点对应的值就是前序遍历数组的第一个元素
+  const rootVal = preorder[preStart];
+  // rootVal 在中序遍历数组中的索引
+  const index = valToIndex.get(rootVal);
+
+  const leftSize = index - inStart;
+
+  // 先构造出当前根节点
+  const root = new TreeNode(rootVal);
+
+  // 递归构造左右子树
+  root.left = build(
+    preorder,
+    preStart + 1,
+    preStart + leftSize,
+    inorder,
+    inStart,
+    index - 1
+  );
+
+  root.right = build(
+    preorder,
+    preStart + leftSize + 1,
+    preEnd,
+    inorder,
+    index + 1,
+    inEnd
+  );
+  return root;
+}
+
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  for (let i = 0; i < inorder.length; i++) {
+    valToIndex.set(inorder[i], i);
+  }
+  return build(
+    preorder,
+    0,
+    preorder.length - 1,
+    inorder,
+    0,
+    inorder.length - 1
+  );
+};
+// 详细解析参见：
+// https://labuladong.github.io/article/?qno=105
