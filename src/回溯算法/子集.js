0a6da92066f32388a6a62814b1012340d459c251
mgl <1375022007@qq.com>
2023-05-01 20:27:29 +0800

    添加组合和集合的变形

--- src/回溯算法/子集.js
+++ src/回溯算法/子集.js
@@ -17,3 +17,39 @@ function subsets(nums) {
   backtrack(0);
   return res;
 }
+
+
+// leetcode 90
+// https://leetcode.cn/problems/subsets-ii/
+// 存在重复元素
+var subsetsWithDup = function(nums) {
+  // 定义结果数组和回溯时的路径数组
+  let res = [];
+  let track = [];
+
+  // 排序，以便于剪枝算法的实现
+  nums.sort((a, b) => a - b); // 让相同元素埃着
+
+  // 回溯算法
+  const backtrack = (nums, start) => {
+      // 前序位置，每个节点的值都是一个子集
+      res.push([...track]);
+
+      // 遍历子集树枝
+      for (let i = start; i < nums.length; i++) {
+          // 剪枝逻辑，值相同的相邻树枝，只遍历第一条
+          if (i > start && nums[i] === nums[i - 1]) {
+              continue;
+          }
+          // 选择当前元素，加入路径数组
+          track.push(nums[i]);
+          // 向子节点递归
+          backtrack(nums, i + 1);
+          // 回溯，撤销选择
+          track.pop();
+      }
+  }
+
+  backtrack(nums, 0);
+  return res;
+};
--- src/回溯算法/组合.js
+++ src/回溯算法/组合.js
@@ -20,3 +20,48 @@ var combine = function (n, k) {
   backtrack(1,n,k);
   return res;
 };
+
+// leetcode 40
+// https://leetcode.cn/problems/combination-sum-ii/
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+  let res = [];
+  let track = [];
+  let trackSum = 0;
+  let sortedCandidates = candidates.sort((a, b) => a - b); // 排序
+  
+  // 回溯函数
+  const backtrack = (nums, start, target) => {
+      // 达到目标和，找到符合条件的组合，记录结果
+      if (trackSum === target) {
+          res.push([...track]);
+          return;
+      }
+      // 先剪枝，超过目标和，直接结束
+      if (trackSum > target) {
+          return;
+      }
+      // 回溯算法标准框架
+      for (let i = start; i < nums.length; i++) {
+          // 剪枝逻辑，值相同的树枝，只遍历第一条
+          if (i > start && nums[i] === nums[i - 1]) {
+              continue;
+          }
+          // 做选择
+          track.push(nums[i]);
+          trackSum += nums[i];
+          // 递归遍历下一层回溯树
+          backtrack(nums, i + 1, target);
+          // 撤销选择
+          track.pop();
+          trackSum -= nums[i];
+      }
+  }
+
+  backtrack(sortedCandidates, 0, target);
+  return res;
+};
